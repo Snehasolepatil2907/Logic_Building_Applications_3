19ce866f0781bbf40f0f7e084ff29a346709a90f
Snehasolepatil2907 <91668207+Snehasolepatil2907@users.noreply.github.com>
2023-02-16 14:08:44 +0530

    Add files via upload

---
+++ Program272.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class MarvellousX
+{
+    public int CapCount(String s)
+    {
+        int iCnt = 0;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+                iCnt++;
+            
+        }
+        return iCnt;
+    }
+}
+
+class Program272
+{
+    public static void main(String Ar[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Please string : ");
+        String str = sobj.nextLine();
+        MarvellousX obj = new MarvellousX();
+        int iRet = obj.CapCount(str);
+        System.out.println("Number of capital case letters are : "+iRet);
+    }
+}
